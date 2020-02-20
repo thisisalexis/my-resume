935731dd0c81b4f0afcfb9d7378c56890b5c6307
Alexis <alexis.ve@gmail.com>
2020-02-19 23:09:52 -0300

    chor: initial commit. Spring boot library added

---
+++ src/main/java/cl/thisisalexis/myresumeorchestrator/MyResumeOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package cl.thisisalexis.myresumeorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResumeOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResumeOrchestratorApplication.class, args);
+	}
+
+}
